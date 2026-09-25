a6d543c951bf8dd4552cc5bc0a94b72072322c48
Felipe Mareque <felipemareque@gmail.com>
2026-09-25 15:51:48 -0300

    restricao do fator de carga para 70%

--- estruturas/tabela_hash.py
+++ estruturas/tabela_hash.py
@@ -5,6 +5,7 @@ class TabelaHash:
         self.tabela = [None] * capacidade
         self.quantidade = 0
         self.colisoes = 0
+        self.limite_fator_carga = 0.7
 
     def _funcao_hash(self, chave):
         valor = 0
@@ -15,16 +16,50 @@ class TabelaHash:
         return valor % self.capacidade
 
     def inserir(self, planeta):
+        self._inserir(planeta)
+
+        if self.fator_carga() >= self.limite_fator_carga:
+            self._rehash()
+
+    def _inserir(self, planeta, contar_colisao=True):
         chave = planeta["nome"]
         indice = self._funcao_hash(chave)
 
         while self.tabela[indice] is not None:
-            self.colisoes += 1
+            if contar_colisao:
+                self.colisoes += 1
+
             indice = (indice + 1) % self.capacidade
 
         self.tabela[indice] = planeta
         self.quantidade += 1
 
-
     def fator_carga(self):
         return self.quantidade / self.capacidade
+
+    def _proxima_capacidade(self):
+        candidato = self.capacidade * 2 + 1
+
+        while True:
+            eh_primo = True
+
+            for divisor in range(2, int(candidato ** 0.5) + 1):
+                if candidato % divisor == 0:
+                    eh_primo = False
+                    break
+
+            if eh_primo:
+                return candidato
+
+            candidato += 2
+
+    def _rehash(self):
+        tabela_antiga = self.tabela
+
+        self.capacidade = self._proxima_capacidade()
+        self.tabela = [None] * self.capacidade
+        self.quantidade = 0
+
+        for planeta in tabela_antiga:
+            if planeta is not None:
+                self._inserir(planeta, False)
--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-from api.swapi import buscar_planetas
-from modelos.planeta import Planeta
-from servicos.armazenamento import salvar_planetas
 from estruturas.tabela_hash import TabelaHash
 
-planetas = buscar_planetas()
-
-planetas_normalizados = []
-
-for dados in planetas:
-    planeta = Planeta.de_dicionario(dados)
-    dados_normalizados = planeta.para_dicionario()
-
-    planetas_normalizados.append(dados_normalizados)
-
-salvar_planetas(planetas_normalizados, "dados/planetas.json")
 
 tabela = TabelaHash()
 
-for planeta in planetas_normalizados:
-    if planeta["nome"] == "Yavin IV" or planeta["nome"] == "Hoth":
-        tabela.inserir(planeta)
+for numero in range(1, 9):
+    planeta = {"nome": f"Planeta {numero}"}
 
-print(tabela.tabela)
-print("Colisões:", tabela.colisoes)
+    tabela.inserir(planeta)
 
+    print(
+        f"Inserção {numero}: "
+        f"quantidade={tabela.quantidade}, "
+        f"capacidade={tabela.capacidade}, "
+        f"fator={tabela.fator_carga()}"
+    )
